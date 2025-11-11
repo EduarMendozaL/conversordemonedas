4093819e5b56dc2136eaf3d9aecb8702af666ce3
EduarMendozaL <edmenlla@gmail.com>
2025-11-11 19:33:09 -0300

    Agregando la clase Principal

---
+++ src/Principal.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        ConsultaApi consulta = new ConsultaApi();
+        DatosDeApi datos = consulta.mostrarDatos("USD", "CLP");
+        System.out.println(datos);
+    }
+}
