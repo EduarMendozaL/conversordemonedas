d9c017a050d7c953fc019afb81ad2594fd8f96f0
EduarMendozaL <edmenlla@gmail.com>
2025-11-11 20:29:28 -0300

    Agregando ingreso por teclado y dando el resultado de la conversión

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         ConsultaApi consulta = new ConsultaApi();
+        double montoAConvertir;
 
         System.out.println("Ingrese moneda de origen: ");
         String monedaDeOrigen = teclado.next();
@@ -11,15 +12,15 @@ public class Principal {
         String monedaDeDestino = teclado.next();
 
         System.out.println("Ingrese el monto a convertir: ");
-        Double montoAConvertir = teclado.nextDouble();
+
+        montoAConvertir = teclado.nextDouble();
 
         DatosDeApi datos = consulta.mostrarDatos(monedaDeOrigen, monedaDeDestino);
 
-        Double valorConvertido = montoAConvertir * datos.conversion_rate();
+        double valorConvertido = montoAConvertir * datos.conversion_rate();
 
-        System.out.println("La conversión del valor " + montoAConvertir + " desde " + monedaDeOrigen + " a " +
-                monedaDeDestino + ", es de: " + valorConvertido);
-        System.out.printf("La conversión del valor %.2f desde %s a %s, es de: %.2f", montoAConvertir, monedaDeOrigen, monedaDeDestino, valorConvertido);
+        System.out.printf("La conversión del valor %.2f desde %s a %s, es de: %.2f",
+                montoAConvertir, monedaDeOrigen, monedaDeDestino, valorConvertido);
 
     }
 }
